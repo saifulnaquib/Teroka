b2fcb8737a7c6ba4d0e1c38a9ce2ad9d623b59af
afifhisham <mafifsempoi@gmail.com>
2022-01-13 03:35:46 +0800

    Changing some UI for LogSign and Register Add username in Register too

--- app/src/main/java/com/example/teroka/LogSign.java
+++ app/src/main/java/com/example/teroka/LogSign.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.teroka.ui.login.LoginActivity;
